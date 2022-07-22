764adb0108a050d62b92965cbdf9ac25059ca5c1
BRIANCHERUIYOT <kipkiruicheruiyot41@gmail.com>
2022-07-22 13:00:31 +0300

    first commit

---
+++ functions.py
@@ -0,0 +1,8 @@
+from this import d
+
+
+def calculate__triangle_area(base,height):
+    return 1/2*(base*height)
+
+def calculate_square_area(length):
+    return length*length
---
+++ my_mainprogram.py
@@ -0,0 +1,3 @@
+import functions as f
+area = f.calculate_square_area(5)
+print(area)
